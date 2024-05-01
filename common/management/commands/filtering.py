cc039da220c6f0f093a7af9fd44af7999d7a759e
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-05-01 17:13:31 +0900

    추천 완료

--- common/management/commands/filtering.py
+++ common/management/commands/filtering.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import pickle
 from django.core.management.base import BaseCommand
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import linear_kernel
 import logging
 
 logger = logging.getLogger('django')
@@ -16,21 +14,14 @@ class Command(BaseCommand):
             logger.info("Start Filtering GET pickle Successfully")
             df1 = pd.read_csv('common/products.csv', encoding='utf-8')
 
-            tfidf = CountVectorizer()
-            tfidf_matrix = tfidf.fit_transform(df1['product_category'])
-
-            cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
             indices = pd.Series(df1.index, index=df1['product_id']).drop_duplicates()
             print(indices)
 
             products = df1[['product_id', 'product_category']].copy()
             pickle.dump(products, open('common/product.pickle', 'wb'))
-            pickle.dump(cosine_sim, open('common/cosine_sim.pickle', 'wb'))
             self.stdout.write(self.style.SUCCESS('Filterring Successfully'))
             logger.info("Filtering Successfully")
         except Exception as e:
             error_message = f"Failed to Filtering to pickle. Error: {str(e)}"
             self.stdout.write(self.style.ERROR(error_message))
             logger.error(error_message)
-
--- common/management/commands/selectLog.py
+++ common/management/commands/selectLog.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import pandas as pd
 import pickle
 from django.core.management.base import BaseCommand
@@ -17,7 +19,9 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         product_pickes = pickle.load(open('common/product.pickle', 'rb'))
-        cosine_sim = pickle.load(open('common/cosine_sim.pickle', 'rb'))
+        # category에 기반하여 벡터화
+        tfidf = CountVectorizer()
+        tfidf_matrix = tfidf.fit_transform(product_pickes['product_category'])
         try:
             logger.info("Start Set Recommend")
             today = timezone.now().date()
@@ -32,23 +36,32 @@ class Command(BaseCommand):
                 logger.info("Today Login User {}".format(user.user_id))
                 reco = recommends.get(user.user_id)
                 if reco is None:
+                    print(user.user_id, "  is None")
                     # Recommend 객체가 없는 경우 새로운 객체 생성
                     create_reco = models.Recommend(created_who=user, product_list="")
                     to_create.append(create_reco)
+                    continue
                 # None이 아닌 모든 product_id를 추출한다.
-                product_ids = models.UserLog.objects.filter(created_who=user.user_id).exclude(
+                product_ids = list(models.UserLog.objects.filter(created_who=user.user_id).exclude(
                     # 필터링 된 결과에서 product_id 필드 값을 리스트 형태로 추출한다.
                     # flat옵션을 통해 결과를 하나의 List로 평탄화하여 반환한다. True가 없다면 튜플들의 리스트 형태로 반환된다.
-                    product_id=None).values_list('product_id', flat=True)
+                    product_id=None).values_list('product_id', flat=True))[:20]
                 products = models.Product.objects.filter(product_id__in=product_ids)
                 categories = [product.product_category for product in products]
                 category_str = ",".join(categories)
+                category_count = Counter(categories)
+                print(user.user_id)
+                print(category_count)
 
-                recommended_products = get_recommend(category_str, product_pickes, cosine_sim)
+                recommended_products = get_recommend(category_str, product_pickes, tfidf, tfidf_matrix)
                 recommended_str = ",".join([str(product) for product in recommended_products])
+                if recommended_products is None:
+                    print(user.user_id)
                 reco.product_list = recommended_str
                 to_update.append(reco)
+            print("created is Not OK")
             models.Recommend.objects.bulk_create(to_create)
+            print("created is OK")
             models.Recommend.objects.bulk_update(to_update, ['product_list'])
             self.stdout.write(self.style.SUCCESS('Successfully'))
             logger.info("SET User recommend Successfully")
@@ -58,24 +71,18 @@ class Command(BaseCommand):
             logger.error(error_message)
 
 
-def get_recommend(category, product_pickes, consine_sim):
-    # category에 기반하여 벡터화
-    tfidf = CountVectorizer()
-    tfidf_matrix = tfidf.fit_transform(product_pickes['product_category'])
-
+def get_recommend(category, products, tfidf, tfidf_matrix):
     # 입력된 category도 같은 vectorizer를 활용해 변환
     category_vector = tfidf.transform([category])
 
-    try:
-        idx = product_pickes[product_pickes['product_category'] == category].index[0]
-    except IndexError:
-        return []
+    # 입력된 category와 모든 products 간의 코사인 유사도 계산
+    consine_sim = linear_kernel(category_vector, tfidf_matrix)
 
-    # 유사도를 기준으로 상위 10개 products를 추천
-    sim_scores = list(enumerate(consine_sim[idx]))
+    # 유사도를 기준으로 상위 30개 products를 추천
+    sim_scores = list(enumerate(consine_sim[0]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:31]
+    sim_scores = sim_scores[:30]
 
     product_indices = [i[0] for i in sim_scores]
 
-    return product_pickes['product_id'].iloc[product_indices]
+    return products['product_id'].iloc[product_indices]
